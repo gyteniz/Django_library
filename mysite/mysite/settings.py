f421368fca16b40e6362051b3ac7d8852f92ae3b
Gytis Vaicius <gyteniz@gmail.com>
2023-04-24 22:26:15 +0300

    Aprasyti bibliotekos modeliai

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'library',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+02:00'
+TIME_ZONE = 'Europe/Vilnius'
 
 USE_I18N = True
 
