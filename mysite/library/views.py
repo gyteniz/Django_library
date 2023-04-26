de53429e3ada97dca0f1d79a8d12f3875b422a57
Gytis Vaicius <gyteniz@gmail.com>
2023-04-26 14:37:45 +0300

    3. sukurtas index viewas, settings.py templates vieta

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_authors = Author.objects.count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
