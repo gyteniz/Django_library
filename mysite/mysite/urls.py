4a8c59af400f34a178d90e18303577c9c5b65117
Gytis Vaicius <gyteniz@gmail.com>
2023-04-26 16:20:02 +0300

    3. pridetas static katalogas ir style.css

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('library/', include('library.urls')),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
