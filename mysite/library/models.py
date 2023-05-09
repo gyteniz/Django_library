5a301fee264c4eb4a1c97393d857da0165429d26
Gytis Vaicius <gyteniz@gmail.com>
2023-05-10 01:52:27 +0300

    8. importuojame LoginRequiredMixin, ir keliame jį į klasės parametrus. Jis django nurodo, kad *view'*sas bus prieinamas tik prisiregistravusiam vartotojui. Jei naudotumėm funkcinį view'są, prieš jį dėtumėm dekoratorių @login_required.
    override'inam get_queryset funkciją ir nurodome, kad Bookinstance modelį filtruosime pagal prisiregistravusį skaitytoją, pagal statusą 'p'(paimta), ir išrūšiuosime pagal due_back lauką.

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -2,6 +2,7 @@ import uuid as uuid
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Genre(models.Model):
@@ -19,7 +20,7 @@ class Genre(models.Model):
 class Author(models.Model):
     first_name = models.CharField(verbose_name="Vardas", max_length=50)
     last_name = models.CharField(verbose_name="Pavarde", max_length=50)
-    description = models.TextField(verbose_name='Aprašymas', max_length=2000, default='')
+    description = HTMLField(verbose_name='Aprašymas', max_length=5000, default='')
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
